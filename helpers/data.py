31a7488ebc19236e64d81a84ff4a4b89300b29a7
Alekseev <darkflashka@yandex.ru>
2025-01-19 18:43:13 +0300

    откат

--- helpers/data.py
+++
@@ -1,8 +0,0 @@
-import random
-class Data:
-    @staticmethod
-    def create_new_email_for_registration():
-        return f'YuriAkimov17{random.randint(100,999)}@yandex.ru'
-    @staticmethod
-    def create_new_password_for_registration():
-        return f'Qwerty{random.randint(100,999)}'
