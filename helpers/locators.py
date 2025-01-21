4bf584a7fc32bcd318579f2e2e41605ace25f61a
Alekseev <darkflashka@yandex.ru>
2025-01-21 18:28:54 +0300

    Исправлена проверка по span  после клика на вкладку в конструктору на новый класс элемента.

--- helpers/locators.py
+++ helpers/locators.py
@@ -28,3 +28,8 @@ class Locators:
     FIRST_SAUCE = (By.XPATH,'//p [@class="BurgerIngredient_ingredient__text__yp3dH"][1]')
     FIRST_BUNS = (By.XPATH , '// p[@class ="BurgerIngredient_ingredient__text__yp3dH"][1]')
     FIRST_FILLING = (By.XPATH, '// p[@class ="BurgerIngredient_ingredient__text__yp3dH"][1]')
+    NEW_LINK_SAUCES_CONSTRUCTOR = (By.XPATH,'//div [@class="tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect"]')
+    NEW_LINK_BUNS_CONSTRUCTOR = (By.XPATH, '//div [@class="tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect"]')
+    NEW_LINK_FILLING_CONSTRUCTOR = (By.XPATH, '//div [@class="tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect"]')
+
+
--- test/test_page_contructor.py
+++ test/test_page_contructor.py
@@ -12,7 +12,7 @@ class TestConstructorPage:
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((Locators.LINK_BUNS_CONSTRUCTOR)))
         driver.find_element(*Locators.LINK_SAUCES_CONSTRUCTOR).click()
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((Locators.FIRST_SAUCE)))
-        assert driver.find_element(*Locators.LINK_SAUCES_CONSTRUCTOR).is_displayed()
+        assert driver.find_element(*Locators.NEW_LINK_SAUCES_CONSTRUCTOR).is_displayed()
 
     def test_click_to_buns_in_constructor(self,driver):
         driver.get(Urls.URL_BASE)
@@ -21,11 +21,11 @@ class TestConstructorPage:
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((Locators.FIRST_SAUCE)))
         driver.find_element(*Locators.LINK_BUNS_CONSTRUCTOR).click()
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((Locators.FIRST_BUNS)))
-        assert driver.find_element(*Locators.LINK_BUNS_CONSTRUCTOR).is_displayed()
+        assert driver.find_element(*Locators.NEW_LINK_BUNS_CONSTRUCTOR).is_displayed()
 
     def test_click_to_filling_in_constructor(self,driver):
         driver.get(Urls.URL_BASE)
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((Locators.LINK_BUNS_CONSTRUCTOR)))
         driver.find_element(*Locators.LINK_FILLING_CONSTRUCTOR).click()
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((Locators.FIRST_FILLING)))
-        assert driver.find_element(*Locators.LINK_FILLING_CONSTRUCTOR).is_displayed()
+        assert driver.find_element(*Locators.NEW_LINK_FILLING_CONSTRUCTOR).is_displayed()
